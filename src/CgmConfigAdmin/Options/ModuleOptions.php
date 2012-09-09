ce1677d127bccaec4bcf3a9de0fb8b5a133719c0
Christopher Martin <cgmartin@gmail.com>
2012-09-09 00:42:48 -0400

    Update license

--- src/CgmConfigAdmin/Options/ModuleOptions.php
+++ src/CgmConfigAdmin/Options/ModuleOptions.php
@@ -4,7 +4,7 @@
  *
  * @link      http://github.com/cgmartin/CgmConfigAdmin for the canonical source repository
  * @copyright Copyright (c) 2012 Christopher Martin (http://cgmartin.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @license   New BSD License https://raw.github.com/cgmartin/CgmConfigAdmin/master/LICENSE
  */
 
 namespace CgmConfigAdmin\Options;
