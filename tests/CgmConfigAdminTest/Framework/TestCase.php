893068f54e85d2e801ea4427cbad8f978cae9a9c
Christopher Martin <cgmartin@gmail.com>
2013-01-12 22:50:58 -0500

    Update copyright year

--- tests/CgmConfigAdminTest/Framework/TestCase.php
+++ tests/CgmConfigAdminTest/Framework/TestCase.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * CgmFeatureAdmin
+ * CgmConfigAdmin
  *
- * @link      http://github.com/cgmartin/CgmFeatureAdmin for the canonical source repository
- * @copyright Copyright (c) 2012 Christopher Martin (http://cgmartin.com)
+ * @link      http://github.com/cgmartin/CgmConfigAdmin for the canonical source repository
+ * @copyright Copyright (c) 2012-2013 Christopher Martin (http://cgmartin.com)
  * @license   New BSD License https://raw.github.com/cgmartin/CgmConfigAdmin/master/LICENSE
  */
 
